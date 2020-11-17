8df372976ea46b78f2a691d8e6401effb5e7019d
ch <mkmsacc@outlook.com>
2020-11-17 23:42:59 +0700

    Refactor test

--- src/test/java/WebClientTest.java
+++ src/test/java/WebClientTest.java
@@ -1,9 +1,36 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class WebClientTest {
 
+	private class TestableWebClient extends WebClient {
+		private HttpURLConnection connection;
+
+		public void setHttpURLConnection(HttpURLConnection connection) {
+			this.connection = connection;
+		}
+
+		public HttpURLConnection createHttpURLConnection(URL url)
+				throws IOException {
+			return this.connection;
+		}
+	}
+
 	@Test
-	void testGetContentOk() {
+	void testGetContentOk() throws Exception {
+		MockHttpURLConnection mockHttpURLConnection = new MockHttpURLConnection();
+		mockHttpURLConnection.setExpectedInputStream(
+				new ByteArrayInputStream("It works".getBytes()));
+		TestableWebClient client = new TestableWebClient();
+		client.setHttpURLConnection(mockHttpURLConnection);
+		String workingContent = client.getContent(new URL("http://localhost"));
+
+		assertEquals("It works", workingContent);
 
 	}
 
